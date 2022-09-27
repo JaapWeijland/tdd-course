124a04c980ade7dbbf2ee2c90eb75f26a5881d88
Robert Durrant <robert.durrant@navara.nl>
2022-09-27 14:22:20 +0200

    Passing number 4 converts to roman numeral

--- exercise4/src/index.js
+++ exercise4/src/index.js
@@ -1,5 +1,6 @@
 function convert(number) {
     let result = "";
+    if (number === 4) return "IV";
     for(let i = 0; i < number; i++) {
         result += "I";
     }
--- exercise4/test/index.spec.js
+++ exercise4/test/index.spec.js
@@ -2,7 +2,11 @@ const assert = require("assert");
 const convert = require("../src/index");
 
 describe("Roman literals", () => {
-    [{ input: 1, output: "I" }, { input: 2, output: "II"}, { input: 4, output: "IV" }].forEach(({ input, output }) => {
+    [
+        { input: 1, output: "I" }, 
+        { input: 2, output: "II"}, 
+        { input: 4, output: "IV" }
+    ].forEach(({ input, output }) => {
         it(`should translate number ${input} to roman literal ${output}`, () => {
             const result = convert(input);
             assert.strictEqual(result, output);
