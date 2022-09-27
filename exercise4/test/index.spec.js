2e8e6d3e7c4b4ac189aeaeec880b37e19df1fada
Robert Durrant <robert.durrant@navara.nl>
2022-09-27 14:11:35 +0200

    Fixed first test, added second test

--- exercise4/test/index.spec.js
+++ exercise4/test/index.spec.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const convert = require("../src/index");
 
 describe("Roman literals", () => {
-    [{ input: 1, output: "I" }].forEach(({ input, output }) => {
+    [{ input: 1, output: "I" }, { input: 2, output: "II"}].forEach(({ input, output }) => {
         it(`should translate number ${input} to roman literal ${output}`, () => {
             const result = convert(input);
             assert(result).strictEqual(output);
